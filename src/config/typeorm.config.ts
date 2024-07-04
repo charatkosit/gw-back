23f3c5039d6209ba250f12b5701c24e7c4d7470f
charatkosit <charatkosit@gmail.com>
2024-07-04 23:22:54 +0700

    rev0.0.3

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,12 +4,12 @@ import { environment } from '../environment/environment'
 
 export const typeOrmConfig: TypeOrmModuleOptions ={
     type: 'mysql',
-    host: environment.dbUrl,
-    port: environment.port,
-    username: environment.dbUsername,
-    password: environment.dbPassword,
-    database: environment.dbName,
+    host: environment.DATABASE_IP,
+    port: environment.DATABASE_PORT,
+    username: environment.DATABASE_USERNAME,
+    password: environment.DATABASE_PASSWORD,
+    database: environment.DATABASE_DB_NAME,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: environment.synchronize,
+    synchronize: environment.SYNCHRONIZE,
     
 }
