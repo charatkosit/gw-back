44f0f9fd259e8b8fc1648eeb7cca5623a048ddb3
charatkosit <charatkosit@gmail.com>
2024-07-05 21:41:09 +0700

    rev0.0.10

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 import helmet from 'helmet';
 
 async function bootstrap() {
+  console.log(`ip_db: ${process.env.DATABASE_IP}`)
+  console.log(`db_username: ${process.env.DATABASE_USERNAME}`)
   const app = await NestFactory.create(AppModule);
 
   app.enableVersioning({
@@ -13,8 +15,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  console.log(`ip_db: ${process.env.DATABASE_IP}`)
-  console.log(`db_username: ${process.env.DATABASE_USERNAME}`)
+
   // app.enableCors({
   //   origin: ['http://172.41.62.164:3000', 'http://172.41.60.18:3000'],
   //   methods: ['GET', 'POST' ,'HEAD', 'PUT', 'PATCH','DELETE'],
